954abb9f2a990c88f73c6bb183e0a319c3ca5066
TicoCR16 <mariojsanchez15@gmail.com>
2020-08-11 11:05:02 -0700

    done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
-const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
+const express = require( "express" );
+const accounts = require( "../accounts/accountsRouter" );
 const server = express();
 
-server.use(express.json());
+server.use( express.json() );
+
+server.use( "/api/accounts", accounts );
+
+server.get( "/", ( req, res ) =>
+{
+  res.status( 200 ).json( "Running..." );
+} );
 
 module.exports = server;
